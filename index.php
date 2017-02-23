bf3a7ff31b2de8ec66d11cb08565300c98c8c4f5
evgrezanov <evgrezanov@gmail.com>
2017-02-24 01:32:03 +0300

    1.2

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
 /*
 Plugin Name: Easy Google Places Reviews
-Plugin URI: en.wphire.ru
+Plugin URI: https://en.wphire.ru
 Description: Display your Google business page reviews on your wordpress pages just by adding a single shortcode. 
-Version: 0.2
-Author: Evgenii rezanov
-Author URI: en.wphire.ru
+Version: 1.2
+Author: Evgenii Rezanov
+Author URI: https://en.wphire.ru
 */
 
 require_once('includes/options_page.php');
 
+add_action( 'wp_enqueue_scripts', 'egpr_register_styles' );
+function egpr_register_styles() {  
+    // Register the style like this for a plugin:  
+    wp_register_style( 'egpr-style', plugins_url( '/includes/css/egpr-style.css', __FILE__ ), array(), '20160213', 'all' );
+    // For either a plugin or a theme, you can then enqueue the style:  
+    wp_enqueue_style( 'egpr-style' );  
+}  
+
+add_action( 'init', 'google_places_review', 0 );
 // Register Custom Post Type
 function google_places_review() {
 
@@ -64,7 +73,6 @@ function google_places_review() {
 	register_post_type( 'google_review', $args );
 
 }
-add_action( 'init', 'google_places_review', 0 );
 
 //cron event
 add_action('wp', 'egpr_activation');
@@ -79,6 +87,7 @@ add_action('egpr_daily_event', 'egpr_get_data');
 add_shortcode('egpr_get_data','egpr_get_data');
 function egpr_get_data () {
 	
+   	// TO DO сделать константой
    	$egpr_url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=";
 	$easy_google_places_review_option_options = get_option( 'easy_google_places_review_option_option_name' ); 
    	$egpr_api = $easy_google_places_review_option_options['google_api_place_id_0'];
@@ -88,6 +97,38 @@ function egpr_get_data () {
 	$egpr_json = $egpr_url . $egpr_api . "&key=" . $egpr_key;
 
 	$egrp_data =  json_decode(file_get_contents($egpr_json));
+    
+    //print_r ($egrp_data);
+    //TO DO понять и отрефакторить
+    $ch = curl_init( $egpr_json );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch, CURLOPT_HEADER, 0 );
+	$data1 = curl_exec( $ch ); // Google response
+	curl_close( $ch );
+	$response = json_decode( $data1, true );
+	print_r($response);
+	$request_url = add_query_arg(
+				array(
+					'photoreference' => $response['result']['photos'][0]['photo_reference'],
+					'key'            => $egpr_key,
+					'maxwidth'       => '300',
+					'maxheight'      => '300',
+				),
+				'https://maps.googleapis.com/maps/api/place/photo' // TO DO сделать константой
+			);
+	//
+	
+	$egpr_formatted_address = $response['result']['formatted_address'];
+	$egpr_international_phone_number = $response['result']['international_phone_number'];
+	$egpr_name = $response['result']['name'];
+	$egpr_weekday_text = $response['result']['opening_hours']['weekday_text'];
+	$egpr_rating = $response['result']['rating'];
+
+	add_option('egpr_formatted_address', $egpr_formatted_address);
+	add_option('egpr_international_phone_number', $egpr_international_phone_number);
+	add_option('egpr_name', $egpr_name);
+	add_option('egpr_weekday_text', $egpr_weekday_text);
+	add_option('egpr_rating', $egpr_rating);
 
 	// TO DO сchange foreach
 	for($i=0; $i<count($egrp_data->result->reviews); $i++) {		
@@ -98,6 +139,7 @@ function egpr_get_data () {
  		$egpr_author_name = $egrp_data->result->reviews[$i]->author_name;
  		$egpr_date = $egrp_data->result->reviews[$i]->time;
  		$egpr_text = $egrp_data->result->reviews[$i]->text;
+ 		$egpr_language = $egrp_data->result->reviews[$i]->language;
  		
 
 		$post = get_page_by_title( $egpr_author_name.'-'.$egpr_date, 'OBJECT', 'google_review' );
@@ -119,6 +161,7 @@ function egpr_get_data () {
 			add_post_meta($post_id, 'egpr_author_name', $egpr_author_name, true);
 			add_post_meta($post_id, 'egpr_date', $egpr_date, true);
 			add_post_meta($post_id, 'egpr_profile_photo_url', $egpr_profile_photo_url, true);
+			add_post_meta($post_id, 'egpr_language', $egpr_language, true);
 
 			//save avatar to db					
 			if(!empty($egpr_profile_photo_url)){
@@ -149,85 +192,25 @@ function egpr_get_data () {
     }
 }			
 
-/*
-add_shortcode('egpr_review_shortcode', 'egpr_review_shortcode');
-function egpr_review_shortcode() {
-
-	//$cacheName = 'easy-google-places-revews';
-	//$reviews_content = get_transient($cacheName);
-
-	//if ($reviews_content === false) {
-	    //ob_start();
-	    $html = '';
-		$args = array(
-		'numberposts' => 5,
-		'post_type'   => 'google_review',
-		'post_status' => 'publish ',
-		);
-
-		$reviews = get_posts( $args );
-
-		if (isset($reviews)) {
-
-			foreach($reviews as $review){ setup_postdata($review);
-				$egpr_author_name = get_post_meta($review->ID, 'egpr_author_name', true);
-				$egpr_date = get_post_meta($review->ID, 'egpr_date', true);
-				$egpr_profile_photo_url = get_post_meta($review->ID, 'egpr_profile_photo_url', true);
-				$egpr_author_url = get_post_meta($review->ID, 'egpr_author_url', true);
-				$egpr_rating = get_post_meta($review->ID, 'egpr_rating', true);
-
-				$egpr_rating_pic = plugins_url('images', __FILE__).'/'.$egpr_rating.'stars.png';
-				if (!isset($egpr_profile_photo_url)) {
-	 				$egpr_profile_photo_url = plugins_url('images', __FILE__).'/profil-pic_dummy.png';
-	 			}
-				// TO DO delete CSS, change style
-
-				 $html .= '<div class="blockquote-list">';
-					$html .= '<blockquote class="clearfix small simple">';
-						$html .= '<div class="quote-thumbnail">';
-							$egpr_avatar = get_the_post_thumbnail( $review->ID, array(150,150) );
-							if (!empty($egpr_avatar)) { $html .= $egpr_avatar; }
-							$html .= '<strong class="quote-title"><a href="'.$egpr_author_url.'"><span class="the-title">'.$egpr_author_name.'</span></a></strong>';
-						$html .= '</div>';
-						$html .= '<div class="quote-text">';
-							$html .= '<div class="quote-content">';
-								$html .= '<img class ="review-rating" src="'.$egpr_rating_pic.'">';
-								$html .= '<br>';
-								$html .= '<span class="review-date">'.date('m/d/Y', $egpr_date).'</span>';
-								$html .= '<br>';
-								$html .= '<em>'.$review->post_content.'</em>';
-							$html .= '</div>';
-						$html .= '</div>';
-					$html .= '</blockquote>';
-				$html .= '</div>';
-			}
-		}
-		else {
-			$html .= 'Cant find reviews!';
-		}
-		wp_reset_postdata();
-		//$reviews_content = ob_get_contents();
-	    //set_transient($cacheName, $reviews_content, DAY_IN_SECONDS);
-	    //ob_end_clean();
-	//}
-	//return $reviews_content;
-	return $html;
-}
-*/
-
-
+// show reviews
 add_shortcode('egpr_review_shortcode', 'egpr_review_shortcode');
 function egpr_review_shortcode() {
 	ob_start();
 	$args = array(
-		'numberposts' => 5,
+		'numberposts' => -1,
 		'post_type'   => 'google_review',
 		'post_status' => 'publish ',
 	);
 
 	$reviews = get_posts( $args );
 
-	if (isset($reviews)) {
+	if (isset($reviews)) { 
+	?>
+		<div class="egpr-reviews-block">
+	<?php
+		$egpr_feedback_url='https://search.google.com/local/writereview?placeid=';
+		$easy_google_places_review_option_options = get_option( 'easy_google_places_review_option_option_name' );
+		$egpr_feedback_url.=$easy_google_places_review_option_options['google_api_place_id_0'];
 
 		foreach($reviews as $review){ setup_postdata($review);
 			$egpr_author_name = get_post_meta($review->ID, 'egpr_author_name', true);
@@ -237,37 +220,80 @@ function egpr_review_shortcode() {
 			$egpr_rating = get_post_meta($review->ID, 'egpr_rating', true);
 
 			$egpr_rating_pic = plugins_url('images', __FILE__).'/'.$egpr_rating.'stars.png';
+			
 			if (!isset($egpr_profile_photo_url)) {
-	 			$egpr_profile_photo_url = plugins_url('images', __FILE__).'/profil-pic_dummy.png';
+	 			
+	 			$egpr_profile_photo_url = plugins_url('images', __FILE__).'/get.jpg';
+	 		
 	 		}
 			?>
-			<div class="blockquote-list">
-				<blockquote class="clearfix small simple">
-					<div class="quote-thumbnail">
-						<?php $egpr_avatar = get_the_post_thumbnail( $review->ID, array(150,150) ); ?>
-						<?php if (!empty($egpr_avatar)) { echo $egpr_avatar; }  ?>
-						<strong class="quote-title"><a href="<?php echo $egpr_author_url; ?>"><span class="the-title"><?php echo $egpr_author_name; ?></span></a></strong>
+			<div class="egpr-review">
+				<div class="egpr-quote-thumbnail">
+					<?php $egpr_avatar = get_the_post_thumbnail( $review->ID, array(150,150), array('class' => 'egpr-avatar') ); ?>
+					<?php if (!empty($egpr_avatar)) { echo $egpr_avatar; }  
+							else {
+					?>
+					<img  class="egpr-unknow-avatar" scr="<?php echo $egpr_profile_photo_url; ?>" />
+					<?php
+						}
+					?>
+					<div class="egpr-quote-title">
+						<span class="egpr-quote-title"><a href="<?php echo $egpr_author_url; ?>"><span class="the-title"><?php echo $egpr_author_name; ?></span></a></span>
+						<br>
+						<span class="egpr-review-date"><?php echo date('m.d.Y', $egpr_date); ?></span>
+						<br>
+						<img class ="egpr-review-rating" src="<?php echo $egpr_rating_pic; ?>">
 					</div>
-					<div class="quote-text">
-						<div class="quote-content">
-							<img class ="review-rating" src="<?php echo $egpr_rating_pic; ?>">
-							<br>
-							<span class="review-date"><?php echo date('m/d/Y', $egpr_date); ?></span>
-							<br>
-							<em><?php echo $review->post_content; ?></em>
-						</div>
-					</div>
-				</blockquote>
+				</div>
+				<div class="egpr-quote-text">
+					<em><?php echo $review->post_content; ?></em>
+				</div>
 			</div>
 			<?php
-			}
+		}
+		?>
+		</div>
+		<?php
 	}
 	else {
 		echo "Cant find reviews!";
 	}
 	
 	wp_reset_postdata();
+	?>
+
+	<a href="<?php echo $egpr_feedback_url; ?>">Rate & Review</a>
+
+	<?php
+	return ob_get_clean();
+}
+
+// show place info
+add_shortcode('egpr_place_info_shortcode', 'egpr_place_info_shortcode');
+function egpr_place_info_shortcode() {
+	ob_start();
+	?>
 	
+	<div class="egpr-place-info">
+		<span class="egpr-name"><?php echo get_option('egpr_name'); ?></span><br>
+		<span class="egpr-address"><?php echo get_option('egpr_formatted_address'); ?></span><br>
+		<span class="egpr-phone-number"><?php echo get_option('egpr_international_phone_number'); ?></span><br>
+		<span class="egpr-rating"><?php echo 'Rating: '.get_option('egpr_rating'); ?></span>
+		<ul class="egpr-weekday-text">
+			<?php 
+				$egpr_weekdays = get_option('egpr_weekday_text');
+				if (isset($egpr_weekdays)) {
+					foreach ($egpr_weekdays as $egpr_weekday) {
+						?>
+						<li class="egpr-weekday-item"><?php echo $egpr_weekday; ?></li>
+						<?php
+					}
+				}
+			?>
+		</ul>	
+	</div>
+	<?php
+
 	return ob_get_clean();
 }
 
